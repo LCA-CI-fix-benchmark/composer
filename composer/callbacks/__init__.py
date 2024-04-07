4eca2baf64a7090fcf80dd919fdfc93e24c99087
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:43:28 +0000

    test_lca_code_editing__code-editing-metrics-x23df044ed4cf8ed8__id_92

--- composer/callbacks/__init__.py
+++ composer/callbacks/__init__.py
@@ -13,12 +13,12 @@ from composer.callbacks.export_for_inference import ExportForInferenceCallback
 from composer.callbacks.free_outputs import FreeOutputs
 from composer.callbacks.generate import Generate
 from composer.callbacks.health_checker import HealthChecker
-from composer.callbacks.image_visualizer import ImageVisualizer
-from composer.callbacks.lr_monitor import LRMonitor
-from composer.callbacks.memory_monitor import MemoryMonitor
-from composer.callbacks.mlperf import MLPerfCallback
-from composer.callbacks.nan_monitor import NaNMonitor
-from composer.callbacks.optimizer_monitor import OptimizerMonitor
+from composer.callbacks.image_visualizer import ImageVisualizer as ComposerImageVisualizer
+from composer.callbacks.lr_monitor import LRMonitor as ComposerLRMonitor
+from composer.callbacks.memory_monitor import MemoryMonitor as ComposerMemoryMonitor
+from composer.callbacks.mlperf import MLPerfCallback as ComposerMLPerfCallback
+from composer.callbacks.nan_monitor import NaNMonitor as ComposerNaNMonitor
+from composer.callbacks.optimizer_monitor import OptimizerMonitor as ComposerOptimizerMonitor
 from composer.callbacks.runtime_estimator import RuntimeEstimator
 from composer.callbacks.speed_monitor import SpeedMonitor
 from composer.callbacks.system_metrics_monitor import SystemMetricsMonitor
