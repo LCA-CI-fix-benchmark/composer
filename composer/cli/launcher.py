17b1cc92ae8a3e8d2b60688c0a7c419e76ec8285
ubuntu <ubuntu@ip-10-0-0-33>
2024-04-23 23:38:25 +0000

    test_lca_code_editing__code-editing-metrics-x3ee70613189c9946__id_95

--- composer/cli/launcher.py
+++ composer/cli/launcher.py
@@ -21,7 +21,15 @@ import psutil
 import torch
 
 import composer
-from composer.utils import get_free_tcp_port
+from composer.utils ifor global_rank, process in processes.items():
+    process.poll()
+    if process.returncode is None:
+        log.error('Global rank %s (PID %s) has still not exited; return exit code 1.', global_rank, process.pid)
+    elif process.returncode != 0:
+        log.error('Global rank %s (PID %s) exited with code %s', global_rank, process.pid, process.returncode)
+        return process.returncode
+
+return 0ree_tcp_port
 
 CLEANUP_TIMEOUT = datetime.timedelta(seconds=30)
 
