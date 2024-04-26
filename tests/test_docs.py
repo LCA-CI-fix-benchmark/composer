df6dd6200d713204a47aadc8a52a439bfb6637e9
ubuntu <ubuntu@ip-10-0-0-52>
2024-04-26 04:12:03 +0000

    test_lca_code_editing__code-editing-metrics-1f948d4c0c3ca9e8__id_94

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -16,13 +16,17 @@ import pytest
 def check_output(proc: subprocess.CompletedProcess):
     # Check the subprocess output, and raise an exception with the stdout/stderr dump if there was a non-zero exit
     # The `check=True` flag available in `subprocess.run` does not print stdout/stderr
-    if proc.returncode == 0:
-        return
-    error_msg = textwrap.dedent(f"""\
-        Command {proc.args} failed with exit code {proc.returncode}.
-        ----Begin stdout----
-        {proc.stdout}
-        ----End stdout------
+    if proc.returncode != 0:
+        error_msg = textwrap.dedent(f"""\
+            Command {proc.args} failed with exit code {proc.returncode}.
+            ----Begin stdout----
+            {proc.stdout}
+            ----End stdout------
+            ----Begin stderr----
+            {proc.stderr}
+            ----End stderr------
+        """)
+        raise RuntimeError(error_msg)
         ----Begin stderr----
         {proc.stderr}
         ----End stderr------""")
