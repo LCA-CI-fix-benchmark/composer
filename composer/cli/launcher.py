61ec496b699d4e6481aeb075ad350c1b454afc1e
ubuntu <ubuntu@ip-10-0-0-188>
2024-05-02 12:23:43 +0000

    test_lca_code_editing__code-editing-metrics-4e92c9b8a087e580__id_92

--- composer/cli/launcher.py
+++ composer/cli/launcher.py
@@ -346,7 +346,7 @@ def _monitor_processes(processes: Dict[int, subprocess.Popen]):
                         process_has_crashed = True
                         break
                     else:
-                        # exited cleanly
+                        # Process exited without errors
                         log.info(f'Rank {global_rank} finished successfully.')
             if process_has_crashed or all_processes_finished:
                 break
@@ -449,7 +449,7 @@ def _aggregate_process_returncode(processes: Dict[int, subprocess.Popen]) -> int
     for global_rank, process in processes.items():
         process.poll()
         if process.returncode is None:
-            log.error('Global rank %s (PID %s) has still not exited; return exit code 1.', global_rank, process.pid)
+            log.error(f'Global rank {global_rank} (PID {process.pid}) has still not exited; return exit code 1.')
             return 1
         if process.returncode != 0:
             log.error('Global rank %s (PID %s) exited with code %s', global_rank, process.pid, process.returncode)
