40b8b6073e7c4b71018ae5858da4f0e45fcf8455
ubuntu <ubuntu@ip-10-0-1-21>
2025-01-10 12:24:27 +0000

    waleko__claude-3.5-sonnet_d33e__id_92

--- tests/callbacks/test_memory_snapshot.py
+++ tests/callbacks/test_memory_snapshot.py
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
-from torch.utils.data import DataLoader
 import pathlib
 
-from composer.callbacks import MemorySnapshot
-from composer.trainer import Trainer
-from tests.common import RandomClassificationDataset, SimpleModel
-from composer.loggers import LoggerDestination
+from torch.utils.data import DataLoader
+
 from composer import State, Trainer
+from composer.callbacks import MemorySnapshot
+from composer.loggers import LoggerDestination
+from tests.common import RandomClassificationDataset, SimpleModel
 
 
 class FileUploaderTracker(LoggerDestination):
