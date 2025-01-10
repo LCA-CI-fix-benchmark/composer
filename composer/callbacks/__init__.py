33f6652e093102ea5abeaaba1f666eb5202648c8
ubuntu <ubuntu@ip-10-0-1-94>
2025-01-10 18:07:38 +0000

    waleko__claude-3-haiku_11f8__id_92

--- composer/callbacks/__init__.py
+++ composer/callbacks/__init__.py
@@ -1,3 +1,5 @@
+import importlib
+
 # Copyright 2022 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
 
@@ -6,6 +8,12 @@
 Each callback inherits from the :class:`.Callback` base class. See detailed description and
 examples for writing your own callbacks at the :class:`.Callback` base class.
 """
+
+# Import all callback classes
+module = importlib.import_module(__name__)
+__all__ = [name for name in dir(module) if name[0] != '_']
+
+# Export all callback classes
 from composer.callbacks.activation_monitor import ActivationMonitor
 from composer.callbacks.checkpoint_saver import CheckpointSaver
 from composer.callbacks.early_stopper import EarlyStopper
@@ -26,18 +34,6 @@ from composer.callbacks.threshold_stopper import ThresholdStopper
 from composer.callbacks.memory_snapshot import MemorySnapshot
 
 __all__ = [
-    'ActivationMonitor',
-    'OptimizerMonitor',
-    'LRMonitor',
-    'MemoryMonitor',
-    'NaNMonitor',
-    'SpeedMonitor',
-    'CheckpointSaver',
-    'MLPerfCallback',
-    'EarlyStopper',
-    'ExportForInferenceCallback',
-    'ThresholdStopper',
-    'ImageVisualizer',
     'HealthChecker',
     'RuntimeEstimator',
     'SystemMetricsMonitor',
@@ -45,3 +41,15 @@ __all__ = [
     'FreeOutputs',
     'MemorySnapshot',
 ]
+
+# Order the imports to match the __all__ list
+from composer.callbacks.activation_monitor import ActivationMonitor
+from composer.callbacks.optimizer_monitor import OptimizerMonitor
+from composer.callbacks.lr_monitor import LRMonitor
+from composer.callbacks.memory_monitor import MemoryMonitor
+from composer.callbacks.nan_monitor import NaNMonitor
+from composer.callbacks.speed_monitor import SpeedMonitor
+from composer.callbacks.checkpoint_saver import CheckpointSaver
+from composer.callbacks.mlperf import MLPerfCallback
+from composer.callbacks.early_stopper import EarlyStopper
+from composer.callbacks.export_for_inference import ExportForInferenceCallback
