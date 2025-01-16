495ced8ad3c67f5786158a02b3aa64197eed3175
ubuntu <ubuntu@ip-10-0-1-33>
2025-01-16 03:24:56 +0000

    waleko__claude-3.5-sonnet_c835__id_95

--- composer/callbacks/eval_output_logging_callback.py
+++ composer/callbacks/eval_output_logging_callback.py
@@ -59,14 +59,12 @@ class EvalOutputLogging(Callback):
         self.destination_file = None
 
     def _write_tables_to_output_dir(self, state: State):
-
-        
         try:
             import pandas as pd
         except ImportError as e:
             raise MissingConditionalImportError(extra_deps_group='pandas',
-                                                conda_package='pandas',
-                                                conda_channel='conda-forge') from e
+                                              conda_package='pandas',
+                                              conda_channel='conda-forge') from e
         # write tmp files
         self.hash.update((str(time.time()) + str(random.randint(0, 1_000_000))).encode('utf-8'))
         tmp_dir = os.getcwd() + '/' + self.hash.hexdigest()
