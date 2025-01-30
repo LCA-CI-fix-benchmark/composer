46316ffded607f2943f9c4eb0c62c6d007540f63
ubuntu <ubuntu@ip-10-0-1-79>
2025-01-30 18:21:10 +0000

    waleko__claude-3-opus_b5eb__id_96

--- composer/callbacks/eval_output_logging_callback.py
+++ composer/callbacks/eval_output_logging_callback.py
@@ -13,11 +13,14 @@ from typing import Callable, Optional
 from torch.utils.data import DataLoader
 
 from composer.core import Callback, State
-from composer.datasets.in_context_learning_evaluation import (InContextLearningCodeEvalDataset,
-                                                              InContextLearningLMTaskDataset,
-                                                              InContextLearningMultipleChoiceTaskDataset,
-                                                              InContextLearningQATaskDataset,
-                                                              InContextLearningSchemaTaskDataset)
+try:
+    from composer.datasets.in_context_learning_evaluation import (InContextLearningCodeEvalDataset,
+                                                                  InContextLearningLMTaskDataset,
+                                                                  InContextLearningMultipleChoiceTaskDataset,
+                                                                  InContextLearningQATaskDataset,
+                                                                  InContextLearningSchemaTaskDataset)
+except ImportError:
+    pass
 from composer.loggers import Logger
 from composer.loggers.console_logger import ConsoleLogger
 from composer.utils import MissingConditionalImportError, dist, maybe_create_object_store_from_uri, parse_uri
